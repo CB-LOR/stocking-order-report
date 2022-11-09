28ab133384efbc00568eb522f82d6d9f98261971
Kevin Meeks <kmeeks.22@gmail.com>
2022-11-09 18:43:11 -0500

    LOR-10: Add update order endpoint (#1)
    * LOR-10: Add endpoint to update order status
    
    * Update template with appropriate reference to the update module

---
+++ orderUpdate/update.py
@@ -0,0 +1,64 @@
+import boto3
+import json
+import traceback
+from datetime import datetime
+from decimal import Decimal
+
+ddb = boto3.resource('dynamodb')
+table = ddb.Table('stocking-orders')
+
+
+def lambda_handler(event, context):
+    status_update = parse_event(event)
+    
+    # Add update ts
+    add_order_timestamp(status_update)
+
+    # Get all data from table
+    order_item = table.update_item(
+        Key={
+            'email': status_update['email']
+        },
+        UpdateExpression='set order_ts=:ts, orderStatus=:os',
+        ExpressionAttributeValues={
+            ':ts': status_update['order_ts'],
+            ':os': status_update['orderStatus']
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    print(order_item)
+ 
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin': '*'
+        },
+        "body": json.dumps(order_item['Attributes'], cls=DecimalEncoder),
+    }
+
+
+class DecimalEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Decimal):
+      return str(obj)
+    return json.JSONEncoder.default(self, obj)
+
+
+def add_order_timestamp(order):
+    order['order_ts'] = str(datetime.now())
+
+
+def parse_event(event):
+    # check incoming values and create order
+    try:
+        body = json.loads(event['body'])
+        return {
+            'email': body['email'],
+            'orderStatus': body['orderStatus']
+        }
+    except Exception as e:
+        print('Failed to get all required attributes', e)
+        print(traceback.format_exc())
+        return None
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,19 +1,21 @@
 from datetime import datetime
 import json
 import pytest
+from orderUpdate.update import parse_event, add_order_timestamp, lambda_handler
 
 
 @pytest.fixture()
-def apigw_event():
+def apigw_real_event():
     """ Generates API GW Event"""
 
     return {
+        "body": '{"email": "Lindaed53@yahoo.com", "orderStatus": "delivered"}',
         "resource": "/{proxy+}",
         "requestContext": {
             "resourceId": "123456",
             "apiId": "1234567890",
             "resourcePath": "/{proxy+}",
-            "httpMethod": "GET",
+            "httpMethod": "POST",
             "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
             "accountId": "123456789012",
             "identity": {
@@ -53,11 +55,93 @@ def apigw_event():
             "Accept-Encoding": "gzip, deflate, sdch",
         },
         "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "GET",
+        "httpMethod": "POST",
         "stageVariables": {"baz": "qux"},
         "path": "/examplepath",
     }
 
 
-def test_test(apigw_event):
-    pass
+@pytest.fixture()
+def apigw_event():
+    """ Generates API GW Event"""
+
+    return {
+        "body": '{"email": "batman@gmail.com", "orderStatus": "delivered"}',
+        "resource": "/{proxy+}",
+        "requestContext": {
+            "resourceId": "123456",
+            "apiId": "1234567890",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "PUT",
+            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+            "accountId": "123456789012",
+            "identity": {
+                "apiKey": "",
+                "userArn": "",
+                "cognitoAuthenticationType": "",
+                "caller": "",
+                "userAgent": "Custom User Agent String",
+                "user": "",
+                "cognitoIdentityPoolId": "",
+                "cognitoIdentityId": "",
+                "cognitoAuthenticationProvider": "",
+                "sourceIp": "127.0.0.1",
+                "accountId": "",
+            },
+            "stage": "prod",
+        },
+        "queryStringParameters": {"foo": "bar"},
+        "headers": {
+            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+            "Accept-Language": "en-US,en;q=0.8",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "CloudFront-Viewer-Country": "US",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+            "X-Forwarded-Port": "443",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "X-Forwarded-Proto": "https",
+            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "Cache-Control": "max-age=0",
+            "User-Agent": "Custom User Agent String",
+            "CloudFront-Forwarded-Proto": "https",
+            "Accept-Encoding": "gzip, deflate, sdch",
+        },
+        "pathParameters": {"proxy": "/examplepath"},
+        "httpMethod": "PUT",
+        "stageVariables": {"baz": "qux"},
+        "path": "/examplepath",
+    }
+
+
+def test_parse_event(apigw_event):
+
+    order = parse_event(apigw_event)
+
+    assert order['email'] == 'batman@gmail.com'
+    assert order['orderStatus'] == "delivered"
+
+
+def test_order_ts():
+    order = {}
+    ts_start = datetime.now()
+    add_order_timestamp(order)
+    ts_end = datetime.now()
+    print(ts_start)
+    print(order.get('order_ts'))
+    print(ts_end)
+    assert order.get('order_ts', None) is not None
+    assert ts_start < datetime.strptime(order.get('order_ts', 0), '%Y-%m-%d %H:%M:%S.%f') < ts_end
+
+
+def test_orderUpdate(apigw_real_event):
+    response = lambda_handler(apigw_real_event, None)
+    assert response.get('statusCode') == 200
+    assert 'body' in response
+    print(response.get('body'))
+
+
